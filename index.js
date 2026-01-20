9e4fb96cff1eafef2708f3faa1537fd48bbff468
AHagan24 <addisonhagan92@gmail.com>
2026-01-20 13:57:03 -0500

    Testing

---
+++ index.js
@@ -0,0 +1,24 @@
+function contact(event) {
+  event.preventDefault();
+  const loading = document.querySelector(".modal__overlay--loading");
+  const success = document.querySelector(".modal__overlay--success");
+  loading.classList += " modal__overlay--visible";
+
+  emailjs
+    .sendForm(
+      "service_thlawti",
+      "template_xe3tubh",
+      event.target,
+      "xBLWdgZbW6QXYvbOi",
+    )
+    .then(() => {
+      loading.classList.remove("modal__overlay--visible");
+      success.classList += " modal__overlay--visible";
+    })
+    .catch(() => {
+      loading.classList.remove("modal__overlay--visible");
+      alert(
+        "The email service is temporarily unavailable. Please contact me directly on addisonhagan917@gmail.com",
+      );
+    });
+}
